3783187499404696045db4d0a47368411de040eb
Kushal <kushalr2001@gmail.com>
2022-04-16 15:42:51 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import pickle
+from flask import Flask, request, jsonify, render_template
+import numpy as np
+
+app = Flask(__name__, template_folder= 'template') #create flask app
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def homepage():
+    return render_template('pred.html')
+
+@app.route('/predict',methods = ['POST']) #model will provide output for inputs
+
+def predict():
+
+
+    int_features = [float(x) for x in request.form.values()]
+    features = [np.array(int_features)]
+    prediction = model.predict(features)
+
+    output = prediction[0]
+
+    return render_template('pred.html', prediction_text = 'Survival Predection: {}'.format(output))
+
+if __name__== "__main__":
+    app.run(debug= True)
